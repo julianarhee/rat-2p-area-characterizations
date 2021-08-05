4a392da10c81a1c8b985f47a8110d90b13c78f75
Juliana Rhee <juliana.rhee@gmail.com>
2021-08-05 15:23:27 -0400

    Nb to script for scatter calculation, single fov

---
+++ analyze2p/scatter.py
@@ -0,0 +1,314 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import glob
+import os
+import cv2
+import glob
+import importlib
+import h5py
+import json
+import copy
+import _pickle as pkl
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import pylab as pl
+import matplotlib as mpl
+import scipy.stats as spstats
+
+import analyze2p.utils as hutils
+import analyze2p.plotting as pplot
+import analyze2p.receptive_fields.utils as rfutils
+import analyze2p.aggregate_datasets as aggr
+import analyze2p.extraction.rois as roiutils
+import analyze2p.retinotopy.utils as retutils
+import analyze2p.retinotopy.segment as seg
+import analyze2p.gradients as grd
+
+
+# In[3]:
+def load_vectors(dk, va, create_new=False):
+    retinorun = grd.get_best_retinorun(dk)
+    gresults = grd.load_gradients(dk, va, retinorun, create_new=create_new)
+    
+    AZMAP_NP = gresults['az_gradients']['image']
+    ELMAP_NP = gresults['el_gradients']['image']
+    GVECTORS = {'az': gresults['az_gradients']['vhat'], 
+                'el': gresults['el_gradients']['vhat']}
+
+    return AZMAP_NP, ELMAP_NP, GVECTORS
+
+def plot_gradients(dk, va, retinorun):
+    # Gradient plot
+    spacing =200
+    scale = 0.0001 #0.0001
+    width = 0.01 #1 #0.01
+    headwidth=20
+    contour_lc='w'
+    contour_lw=1
+
+    # load
+    gresults = grd.load_gradients(dk, va, retinorun, create_new=False)
+    grad_az = gresults['az_gradients']
+    grad_el = gresults['el_gradients']
+    AZMAP_NP = grad_az['image']
+    ELMAP_NP = grad_el['image']
+    area_mask = gresults['area_mask']
+
+    #### Plot gradients
+    fig = seg.plot_gradients_in_area(area_mask, AZMAP_NP, ELMAP_NP, 
+                         grad_az, grad_el, cmap_phase=cmap,
+                         contour_lc=contour_lc, contour_lw=contour_lw, 
+                         spacing=spacing, 
+                         scale=scale, width=width, headwidth=headwidth)
+    pl.subplots_adjust(left=0.1, right=0.9, bottom=0.2, hspace=0.8, top=0.8)
+    fig.text(0.05, 0.95, 'Gradients, est. from MOVINGBAR (%s)' % dk)
+    pl.savefig(os.path.join(curr_dst_dir, 'np_gradients.svg'))
+
+    return fig
+
+
+def visualize_scatter_on_fov(df_, AZMAP_NP, ELMAP_NP, zimg_r=None,
+                cmap='Spectral', markersize=50, lw=0.6, alpha=1., 
+                plot_true=True, plot_predicted=True, plot_lines=True):
+
+    # Make sure we are in bounds of FOV
+    max_ypos, max_xpos = AZMAP_NP.shape
+    incl_plotdf = df_[(df_['predicted_ml_pos']>=0) \
+                    & (df_['predicted_ml_pos']<=max_xpos)\
+                    & (df_['predicted_ap_proj']>=0) \
+                    & (df_['predicted_ap_proj']<=max_ypos)].copy()
+    excl_ixs = [i for i in df_.index.tolist() if i not in incl_plotdf.index]
+    #plotdf = df_.loc[excl_ixs].copy()
+    #plotdf = incl_plotdf.iloc[0::].copy()
+    plotdf=df_.copy()
+
+    fig, axn = pl.subplots(1, 2, figsize=(7,5))
+    for ax, cond in zip(axn, ['azimuth', 'elevation']):
+        neuropil_map = AZMAP_NP.copy() if cond=='azimuth' else ELMAP_NP.copy()
+        retino_label='x0' if cond=='azimuth' else 'y0'
+        # Set color limits
+        vmin = min([np.nanmin(neuropil_map), plotdf[retino_label].min()])
+        vmax = max([np.nanmax(neuropil_map), plotdf[retino_label].max()])
+        normalize = mpl.colors.Normalize(vmin=vmin, vmax=vmax)
+        ax.set_title(cond)
+        if zimg_r is not None:
+            ax.imshow(zimg_r, cmap='gray') #, vmin=abs_vmin, vmax=abs_vmax)
+        ax.imshow(neuropil_map, cmap=cmap, vmin=vmin, vmax=vmax, alpha=alpha)
+        if plot_true:
+            # Plot soma
+            sns.scatterplot(x='ml_pos', y='ap_pos', data=plotdf, ax=ax,
+                    alpha=alpha, hue=retino_label, hue_norm=normalize, 
+                    palette=cmap,
+                    s=markersize, linewidth=lw, edgecolor='k', zorder=1000) 
+        if plot_predicted:
+            # Plot soma
+            sns.scatterplot(x='predicted_ml_pos', y='predicted_ap_pos', 
+                    data=plotdf, ax=ax,
+                    alpha=alpha, hue=retino_label, hue_norm=normalize, 
+                    palette=cmap,
+                    s=markersize, linewidth=lw, edgecolor='w', zorder=1000) 
+        if plot_lines:
+            # Plot connecting line
+            for (x1, y1), (x2, y2) in zip(plotdf[['predicted_ml_pos', \
+                                                  'predicted_ap_pos']].values,
+                           plotdf[['ml_pos', 'ap_pos']].values):
+                ax.plot([x1, x2], [y1, y2], lw=0.5, markersize=0, color='k')
+    for ax in axn:
+        ax.legend_.remove()
+        ax.axis('off')
+
+    return fig
+
+
+
+
+
+rootdir = '/n/coxfs01/2p-data'
+aggregate_dir = '/n/coxfs01/julianarhee/aggregate-visual-areas'
+
+#### Select dataset and create output dirs
+
+dk = '20190617_JC097_fov1'
+va = 'V1'
+experiment='rfs'
+
+
+### RFs, Select parameters
+traceid='traces001'
+response_type='dff'
+is_neuropil=True
+do_spherical_correction=False
+fit_thr=0.5
+
+# NP background calc.
+np_mag_thr=0.001
+np_delay_map_thr=1.0
+np_ds_factor=2
+
+# Smoothing (NEUROPIL)
+target_sigma_um=20
+desired_radius_um=20
+smooth_spline_x=1
+smooth_spline_y=1
+create_new=False
+redo_smooth=True
+is_neuropil=True
+ds_factor=1
+
+
+#### Some plotting stuff
+scatter_kws={'s':2}
+cmap ='Spectral'
+
+# FOV scatter coords 
+markersize=50
+lw=0.6
+alpha=1
+plot_true=True
+plot_predicted=True
+plot_lines=True
+
+do_gradients=False
+do_model=False
+
+#### Select output dirs
+session, animalid, fovn = hutils.split_datakey_str(dk)
+fovdir = glob.glob(os.path.join(rootdir, animalid, session, \
+                'FOV%i_*' % fovn))[0]
+curr_dst_dir = os.path.join(fovdir, 'segmentation')
+if not os.path.exists(curr_dst_dir):
+    os.makedirs(curr_dst_dir)
+
+#### Load NEUROPIL BACKGROUND and GRADIENTS
+AZMAP_NP, ELMAP_NP, GVECTORS = load_vectors(dk, va, create_new=do_gradients)
+if plot:
+    fig = plot_gradients(dk, va, retinorun)
+    pl.savefig(os.path.join(curr_dst_dir, 'np_gradients.svg'))
+    pl.close()
+
+#### Use NEUROPIL to estimate linear model
+try:
+    REGR_NP = grd.load_models(dk, va, rootdir=rootdi)
+    assert REGR_NP is not None
+except Exception as e:
+    plot_aligned=True
+    do_model=True
+
+if do_model:
+    # 1. Get retino data for NEUROPIL (background)
+    retinodf_np = grd.get_neuropil_data(dk, retinorun, mag_thr=np_mag_thr, 
+                                delay_map_thr=np_delay_map_thr, 
+                                ds_factor=np_ds_factor)
+    # 2. Align FOV to gradient vector direction 
+    aligned_, M = grd.align_cortex_to_gradient(retinodf_np, GVECTORS,
+                                      xlabel='ml_pos', ylabel='ap_pos')
+    aligned_np = pd.concat([retinodf_np, aligned_], axis=1).dropna()
+    # 3. Fit model
+    REGR_NP = grd.regress_cortex_and_retino_pos(aligned_np, \
+                    xvar='proj', model='ridge')
+    regr_np_meas = grd.regress_cortex_and_retino_pos(aligned_np, \
+                    xvar='pos', model='ridge')
+    # Save
+    grd.update_models(dk, va, REGR_NP)
+    if verbose:
+        print("NEUROPIL, MEASURED:")
+        print(regr_np_meas.to_markdown())
+        print("NEUROPIL, ALIGNED:")
+        print(REGR_NP.to_markdown())
+    fig = grd.plot_measured_and_aligned(aligned_np, REGR_NP, regr_np_meas)
+    fig.text(0.01, 0.95, 'Aligned CTX. vs retino\n(BAR, Neuropil, %s)' % dk)
+    figname = 'measured_vs_aligned_NEUROPIL'
+    pl.save_fig(os.path.join(curr_dst_dir, '%s.svg' % figname))
+    pl.close()
+
+#### Load soma
+df_soma = grd.get_soma_data(dk, experiment=experiment, retinorun=retinorun, 
+                            protocol='TILE', traceid=traceid,
+                            response_type=response_type,
+                            do_spherical_correction=do_spherical_correction)
+#### Align soma coords to gradient
+aligned_, M = grd.align_cortex_to_gradient(df_soma, GVECTORS,
+                                  xlabel='ml_pos', ylabel='ap_pos')
+aligned_soma = pd.concat([df_soma, aligned_], axis=1).dropna()\
+                    .reset_index(drop=True)
+#### Align SOMA coords
+regr_soma_meas = grd.regress_cortex_and_retino_pos(aligned_soma, 
+                                                   xvar='pos', model='ridge')
+regr_soma_proj = grd.regress_cortex_and_retino_pos(aligned_soma, 
+                                                    xvar='proj', model='ridge')
+if verbose:
+    print("SOMA, MEASURED:")
+    print(regr_soma_meas.to_markdown())
+    print("SOMA, ALIGNED:")
+    print(regr_soma_proj.to_markdown())
+if plot:
+    # PLOT, soma
+    fig = grd.plot_measured_and_aligned(aligned_soma, 
+                        regr_soma_proj, regr_soma_meas)
+    fig.text(0.01, 0.95, 'Measured vs Aligned CTX to RETINO pos (%s)' % dk)
+    figname = 'compared_measured_vs_aligned_CTX_to_RETINO'
+    pl.savefig(os.path.join(curr_dst_dir, '%s.svg' % figname))
+    pl.close()
+
+#### Predict CORTICAL position (from retino position)
+p_x = grd.predict_cortex_position(REGR_NP, cond='az', 
+                          points=aligned_soma['x0'].values)
+p_y = grd.predict_cortex_position(REGR_NP, cond='el', 
+                          points=aligned_soma['y0'].values)
+aligned_soma['predicted_ml_proj'] = p_x
+aligned_soma['predicted_ap_proj'] = p_y
+
+#### Predict RETINO position (from cortical position)
+r_x = grd.predict_retino_position(REGR_NP, cond='az', 
+                          points=aligned_soma['ml_proj'].values)
+r_y = grd.predict_retino_position(REGR_NP, cond='el', 
+                          points=aligned_soma['ap_proj'].values)
+aligned_soma['predicted_x0'] = r_x
+aligned_soma['predicted_y0'] = r_y
+
+if plot:
+    #### Calculate inverse for visualizing on FOV
+    pred_INV = [np.linalg.inv(M).dot(np.array([x, y])) for (x, y) \
+                in aligned_soma[['predicted_ml_proj', 'predicted_ap_proj']].values]
+    pred_inv_df = pd.DataFrame(pred_INV, columns=['pred_inv_x', 'pred_inv_y'], 
+                          index=aligned_soma.index)
+    aligned_soma['predicted_ml_pos'] = pred_inv_df['pred_inv_x']
+    aligned_soma['predicted_ap_pos'] = pred_inv_df['pred_inv_y']
+
+    # # Visualization
+    zimg, masks, ctrs = roiutils.get_masks_and_centroids(dk, traceid=traceid)
+    pixel_size = hutils.get_pixel_size()
+    zimg_r = retutils.transform_2p_fov(zimg, pixel_size)
+
+    df_ = aligned_soma.copy().reset_index(drop=True)
+    fig = visualize_scatter_on_fov(df_, AZMAP_NP, ELMAP_NP, zimg_r=zimg_r,
+                    cmap=cmap, markersize=markersize, lw=lw, alpha=alpha,
+                    plot_true=plot_true,plot_predicted=predicted, 
+                    plot_lines=plot_lines)
+    fig.text(0.01, 0.95, 'CTX vs RETINO positions - MEASURED (%s)' % dk)
+    pl.savefig(os.path.join(curr_dst_dir, 'fov_true_v_predicted_scatter.svg'))
+    pl.close()
+
+# # Calculate scatter
+deviations = grd.get_deviations(aligned_soma)
+if plot:
+    # Plot
+    fig, axn = pl.subplots(1,2, figsize=(6.5, 3))
+    ax=axn[0]
+    sns.histplot(deviations, x='deg_scatter', hue='axis', ax=ax,
+                stat='probability', cumulative=True )
+    ax.set_title('Retino scatter (deg)')
+    ax=axn[1]
+    sns.histplot(deviations, x='dist_scatter', hue='axis', ax=ax,
+                stat='probability', cumulative=True)
+    ax.set_title('Cortical scatter (um)')
+    pl.subplots_adjust(left=0.1, right=0.8, bottom=0.25, top=0.85, 
+                        wspace=0.5, hspace=0.5)
+    pl.savefig(os.path.join(curr_dst_dir, 'deviations.svg'))
+    pl.close()
+
